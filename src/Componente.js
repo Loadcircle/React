e06bf0eba57bdb7e5af3ec9b4882b49042e438a0
Jesus Milano <jesusmilano96@gmail.com>
2019-12-25 16:44:08 -0500

    Primer Commit del proyecto

---
+++ src/Componente.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Componente = () => (
+    <>
+      <div className="clase">
+        <h1>Titulo</h1>
+        <p>Parrafo</p>
+      </div>
+    </>
+);
+
+export default Componente
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+let target = document.getElementById('root');
+
+// let element = React.createElement("h1", {className: 'saludo'}, 'Hola Mundo');
+
+// ReactDOM.render(element, target)
+
+ReactDOM.render(<App />, target);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
