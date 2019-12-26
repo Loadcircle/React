f295338c3d539d50b4293bf65bd6be81c671c1fb
Jesus Milano <jesusmilano96@gmail.com>
2019-12-25 20:49:48 -0500

    .

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import "./styles/styles.scss"
 import Componente from './Componente'
+import { func } from 'prop-types';
+
+const items = [
+  { 'title' : 'Hola',
+    'img'   : 'https://via.placeholder.com/350x150' ,
+    'price' : '40'
+  },
+  { 'title' : 'Hola2',
+    'img'   : 'https://via.placeholder.com/350x150' ,
+    'price' : '401'
+  },
+  { 'title' : 'Hola3',
+    'img'   : 'https://via.placeholder.com/350x150' ,
+    'price' : '402'
+  },
+]
 
 const App = () => (
-    <div>
-      <Componente />
-      <Componente />
-      <Componente />
+    <div className="ed-grid m-grid-3">
+      <Componente 
+        title="React desde cero" 
+        img="https://via.placeholder.com/350x150"
+        price="25"/>
+      <Componente 
+        title="React Chafa" 
+        img="https://via.placeholder.com/350x350"
+        price="35"/>
+      <Componente 
+        title="React menos chafa" 
+        img="https://via.placeholder.com/350x250"
+        price="40"/>
+
+        {items.map(c => <Componente  
+        title={c.title}
+        img={c.img}
+        price={c.price}/>)}
+
     </div>
   );
 
--- src/Componente.js
+++ src/Componente.js
@@ -1,12 +1,46 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
-const Componente = () => (
-    <>
-      <div className="clase">
-        <h1>Titulo</h1>
-        <p>Parrafo</p>
-      </div>
-    </>
+const persona = {'nombre' : 'Alberto', 'apellido' : 'Vera', 'edad' : '23'};
+
+const mayorDeEdad = edad => edad > 18; 
+
+const Componente =  props => (
+    <div className="lg-50 lg-to-center">
+        <div className="s-ratio-16-9 img-container s-radius-tl s-radius-tr">
+            <img src={props.img}  alt="lala"/>
+        </div>
+        {}
+        <div className="s-border s-radius-br s-radius-bl s-shadow-bottom">
+            <div className="s-pxy-2">
+                <h3>{ mayorDeEdad(persona.edad) ? <span>Señorito</span> : <span>Nene</span>  }</h3>
+                <p className="s-mb-0">
+                {`hola, me llamo ${persona.nombre} y tengo ${persona.edad} yearsold`}  
+                </p>
+            </div>
+            <footer className="s-cross-center s-bg-grey s-pxy-2">
+                <div className="s-10 s-mr-1">
+                <div className="circle img-container">
+                    <img  src={props.img} alt="lala"/>
+                </div>
+                </div>
+                <p className="s-mb-0">Prof. { `${persona.nombre} ${persona.apellido}` }</p>
+                <div className="button s-to-right">${props.price}USD</div>
+            </footer>
+        </div>
+    </div>
 );
+
+Componente.propTypes = {
+  title: PropTypes.string,
+  image: PropTypes.string,
+  price: PropTypes.string
+}
+
+Componente.defaultProps = {
+  title: 'No se encontro titulo',
+  img: 'https://via.placeholder.com/350x350',
+  price: '00'
+}
 
 export default Componente
